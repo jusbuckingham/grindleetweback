2091d04d1490938bdd65a6ebd3fbf120d012a401
jus <jus.buckingham@gmail.com>
2023-04-03 11:00:55 -0700

    25

---
+++ 25. Reverse Nodes in k-Group.py
@@ -0,0 +1,36 @@
+# Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+# k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+# You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        a = b = head
+        for _ in range(k):
+            if not b:
+                return a
+            b = b.next
+            
+        new_head = self.reverse(a, b)
+        a.next = self.reverseKGroup(b, k)
+        return new_head
+    
+    def reverse(self, a, b):
+        pre = None
+        cur = a
+        nxt = a
+        while cur != b:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+            
+        return pre
